c9d584720ebe22515ee5de911074e133c01fbc5b
catdoglove <34932357+catdoglove@users.noreply.github.com>
2023-04-14 16:04:53 +0900

    보트 애니메이션 구현

--- _Script/example.cs
+++ _Script/example.cs
@@ -4,25 +4,89 @@ using UnityEngine;
 
 public class example : MonoBehaviour
 {
+    private Rigidbody2D rigid2D;
     public Animator boatAni;
+    bool onetime = true;
+    int mapwhere = 88; //보트맵
+
+    float moveX;
+    public GameObject boatSpr, hiddenSpr,backBG,frontBG;
+
     // Start is called before the first frame update
     void Start()
     {
+        rigid2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
+    { 
+        if (mapwhere == 99)
+        {
+            Invoke("boatAnimation", 2f);
+        }
+        else if (mapwhere == 88)
+        {
+            smallBoatGO();
+        }
+    }
+
+    void smallBoatGO()
     {
-        Invoke("boatAnimation", 2f);
+        moveX = 0f;
+        /*
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveX -= 0.07f; boatSpr.GetComponent<SpriteRenderer>().flipX = true;
+            if (transform.position.x <= 0f) //위치 도달 이후 부터 ~~
+            {
+                frontBG.transform.Translate(new Vector3(moveX, 0, 0) * -0.09f);
+                backBG.transform.Translate(new Vector3(moveX, 0, 0) * -0.06f);
+            }
+        }
+        */
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveX += 0.07f; boatSpr.GetComponent<SpriteRenderer>().flipX = false;
+
+            if (transform.position.x >= -6f) //위치 도달 이후 부터 ~~
+            {
+                frontBG.transform.Translate(new Vector3(moveX, 0, 0) * -0.09f);
+                backBG.transform.Translate(new Vector3(moveX, 0, 0) * -0.06f);
+            }
+
+        }
+
+
+        transform.Translate(new Vector3(moveX, 0, 0) * 0.1f);
+
+
+        float ff = 0;
+        if (transform.position.x >= 4.5f) //위치 도달 이후 부터 ~~
+        {
+            ff--;
+            hiddenSpr.transform.Translate(new Vector3(0, ff, 0) * 0.08f);
+        }
+
     }
 
     void boatAnimation()
     {
-        boatAni.Play("ani_boat_up");
-        StartCoroutine("Action_go");
+        while (onetime)
+        {
+            boatAni.Play("ani_boat_ready");
+            onetime = false;
+        }
+        Invoke("boatGOani", 2f);
+
 
     }
 
+    void boatGOani()
+    {
+        boatAni.Play("ani_boat_up");
+        StartCoroutine("Action_go");
+    }
 
     IEnumerator Action_go()
     {
