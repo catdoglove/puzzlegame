7859c04859d77822dd41ec8fd26ddb6dbffddcbd
catdoglove <34932357+catdoglove@users.noreply.github.com>
2023-12-19 15:17:28 +0900

    막대기 미니창 구현, 다리대화수정

---
+++ _Script/StickMIni.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickMIni : MonoBehaviour
+{
+    public GameObject stickWin_obj,sap1_obj, sap2_obj, stick1_obj, stick2_obj, stickKey1_obj, stickKey2_obj, stickKey3_obj;
+    public Sprite[] stick_spr;
+    public int a_i;
+    //public GameObject SGM,CPGM, CPSGM;
+    public int a = 0;
+    public int b = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    /*
+     * W > 2번
+     * D > 2번
+     * A > 2번
+    */
+
+    private void OnEnable()
+    {
+        stick1_obj.GetComponent<SpriteRenderer>().sprite = stick_spr[0];
+        stick2_obj.GetComponent<SpriteRenderer>().sprite = stick_spr[0];
+
+        a = 0;
+        a_i = 0;
+        StopCoroutine("Press");
+        StartCoroutine("Press");
+      
+    }
+
+
+    void WPress()
+    {
+        sap1_obj.SetActive(false);
+        sap2_obj.SetActive(false);
+        stick2_obj.GetComponent<SpriteRenderer>().sprite = stick_spr[1];
+        stick1_obj.GetComponent<SpriteRenderer>().sprite = stick_spr[1];
+    }
+
+
+
+    void APress()
+    {
+        stick2_obj.GetComponent<SpriteRenderer>().sprite = stick_spr[3];
+        stickKey2_obj.SetActive(false);
+    }
+
+    void DPress()
+    {
+        stick1_obj.GetComponent<SpriteRenderer>().sprite = stick_spr[3];
+        stickKey1_obj.SetActive(false);
+    }
+
+
+    IEnumerator Press()
+    {
+        while (a==0)
+        {
+            if (a_i >= 5)
+            {
+                yield return new WaitForSeconds(0.2f);
+                stickWin_obj.SetActive(false);
+                if (b==0)
+                {
+                 //   CPGM.GetComponent<CheckPlayer>().EventNum_i[CPGM.GetComponent<CheckPlayer>().num] = 15;
+                 //   CPGM.GetComponent<CheckPlayer>().EventSetting();
+                    b = 1;
+                }
+                else
+                {
+                 //   CPSGM.GetComponent<CheckPlayer>().EventNum_i[CPGM.GetComponent<CheckPlayer>().num] = 15;
+                 //   CPSGM.GetComponent<CheckPlayer>().EventSetting();
+                }
+                a = 1;
+            }
+            else
+            {
+
+                if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    if (a_i <= 1)
+                    {
+                        ///   SGM.GetComponent<SoundEvt>().auSE.GetComponent<AudioSource>().pitch = 1f;
+                        // SGM.GetComponent<SoundEvt>().soundstick();
+                        WPress();
+                        a_i++;
+                        yield return new WaitForSeconds(0.8f);
+                    }
+
+                }
+
+
+                if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    if (a_i > 1 && a_i < 4)
+                    {
+                    ///   SGM.GetComponent<SoundEvt>().auSE.GetComponent<AudioSource>().pitch = 1f;
+                       // SGM.GetComponent<SoundEvt>().soundstick();
+                        APress();
+                        a_i++;
+                        yield return new WaitForSeconds(0.8f);
+                    }
+
+                }
+
+                if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    if (a_i >= 4)
+                    {
+                     //   SGM.GetComponent<SoundEvt>().auSE.GetComponent<AudioSource>().pitch = 1f;
+                       // SGM.GetComponent<SoundEvt>().soundstick();
+                        DPress();
+                        a_i++;
+                        yield return new WaitForSeconds(0.8f);
+                    }
+                }
+
+
+
+            }
+
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+
+
+}
