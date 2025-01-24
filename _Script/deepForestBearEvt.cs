e981c7b1243652c818824d156543b64bcb15cb7f
catdoglove <34932357+catdoglove@users.noreply.github.com>
2025-01-24 16:11:44 +0900

    깊은숲 마무리, 쥐 거미줄

---
+++ _Script/deepForestBearEvt.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deepForestBearEvt : MonoBehaviour
+{
+    public Animator BearAni;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        BearAni.Play("ani_npc_deepbear_appear");
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Invoke("changeNormal",5f);
+    }
+
+    void changeNormal()
+    {
+        BearAni.Play("ani_npc_deepbear_normal");
+    }
+}
