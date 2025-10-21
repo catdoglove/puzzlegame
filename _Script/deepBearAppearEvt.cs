257c51d25ed0d90a05eaca2df4b35a22a0ff07ef
catdoglove <34932357+catdoglove@users.noreply.github.com>
2025-10-21 16:06:53 +0900

    깊은숲마무리 그래픽

---
+++ _Script/deepBearAppearEvt.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deepBearAppearEvt : MonoBehaviour
+{
+    public GameObject bearEvt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetInt("deepBearAppear", 0);
+    }
+
+    private void Update()
+    {
+        if (PlayerPrefs.GetInt("deepBearAppear", 0) == 1)
+        {
+            bearEvt.SetActive(true);  //임시??
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        PlayerPrefs.SetInt("deepBearAppear", 1);
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        PlayerPrefs.SetInt("deepBearAppear", 0);
+    }
+}
